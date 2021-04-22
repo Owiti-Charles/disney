17e952d86a2cb53b9f730c0b090d41f31f178d6c
Owiti-Charles <mikeycharlesm7@gmail.com>
2021-04-22 12:23:50 +0300

    Set up project and login page

---
+++ src/components/Login.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components'; 
+
+const Login = (props) => {
+   return(
+        <Container>
+            <Content>
+                content
+            </Content>
+        </Container>
+   )
+}
+
+const Container = styled.section`
+overflow: hidden;
+display: flex;
+flex-direction: column;
+text-align: center;
+height: 100vh;
+`;
+const Content = styled.div`
+margin-bottom: 10vh;
+width: 100%;
+position: relative;
+min-height: 100vh;
+box-sizing: border-box;
+display: flex;
+justify-content:center;
+align-items: center; 
+flex-direction: column;
+padding: 80px 40px;
+height: 100%; 
+
+`;
+
+export default Login;
