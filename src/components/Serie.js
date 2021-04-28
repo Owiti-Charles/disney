bc61477fa27e8e874599b09235b0e5afb428ee59
Owiti-Charles <mikeycharlesm7@gmail.com>
2021-04-28 21:11:02 +0300

    Fix styles in series page

--- src/components/Serie.js
+++ src/components/Serie.js
@@ -12,8 +12,8 @@ const Serie = () => {
             <h4>Popular TV Shows</h4>
             <Content>
                 {movies && movies.map((movie, key) => (
-                <Wrap key={key}> {movie.id}
-                    <Link to={`/series-detail/`+ movie.id}>
+                <Wrap key={key}>
+                    <Link to={`/series-detail/` + movie.id}>
                             <img src={`${image_url}${movie.poster_path}`} alt={movie.name} />
                     </Link>
                  </Wrap>
@@ -33,30 +33,32 @@ const Content = styled.div`
   display: grid;
   grid-gap: 25px;
   gap: 25px;
-  grid-template-columns: repeat(4, minmax(0, 1fr));
+  grid-template-columns: repeat(5, minmax(0, 1fr));
   @media (max-width: 768px) {
     grid-template-columns: repeat(2, minmax(0, 1fr));
   }
 `;
 const Wrap = styled.div`
-  padding-top: 56.25%;
+//   padding-top: 56.25%;
   border-radius: 10px;
   box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
     rgb(0 0 0 / 73%) 0px 16px 10px -10px;
   cursor: pointer;
   overflow: hidden;
   position: relative;
+  display: flex;
+  top: 0;
+  left: 0;
   transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
   border: 3px solid rgba(249, 249, 249, 0.1);
   img {
     inset: 0px;
-    display: block;
-    height: 100%;
     object-fit: cover;
     opacity: 1;
-    position: absolute;
-    transition: opacity 500ms ease-in-out 0s;
+    display: inline-block;
     width: 100%;
+    height: 100%;
+    transition: opacity 500ms ease-in-out 0s;
     z-index: 1;
     top: 0;
   }
